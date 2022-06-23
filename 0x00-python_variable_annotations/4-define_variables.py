87593207a86d42ce26a23e18c891e2370ab07940
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-06-23 19:07:34 +0300

    fix doc error

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -2,7 +2,7 @@
 """Define variables using annotations"""
 
 
-a: int(1)
-pi: float(3.14)
-i_understand_annotations: bool(True)
-school: str("Holberton")
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
