329dbb09a1cdf97774683572e47cf0823cec763c
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-06-28 13:22:09 +0300

    add type annonation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     for x in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
