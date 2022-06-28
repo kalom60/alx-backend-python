89d722c2f363a8b268b9f1e32868f1b482ebd0b8
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-06-28 13:23:43 +0300

    add doc

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Async Generator"""
 
 import asyncio
 import random
@@ -6,6 +7,7 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
+    """return random float number using yield generator"""
     for x in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
