cea6b979150e9df8b000b76ab8cc8f9eea7e4124
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-06-28 13:31:04 +0300

    Async Comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Async Comprehensions"""
+
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """return list of float numbers"""
+    return [x async for x in async_generator()]
